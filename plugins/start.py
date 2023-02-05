623d49a6266703f9ced198bcc5577f9b913f5675
vkthouseef <122280249+vkthouseef@users.noreply.github.com>
2023-02-05 21:50:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -37,7 +37,7 @@ async def start(client, message):
     user = message.from_user
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id)             
-    txt=f"👋 Hai {user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 & 𝙲𝚞𝚜𝚝𝚘𝚖 𝙲𝚊𝚙𝚝𝚒𝚘𝚗 𝚂𝚞𝚙𝚙𝚘𝚛𝚝!"
+    txt=f"**👋 Hai {user.mention}** \n**I'm A Simple File Rename+File To Video Converter Bot With Permanent Thumbnail !**"
     button=InlineKeyboardMarkup([[
         InlineKeyboardButton("👼 DEV 👼", callback_data='dev')
         ],[
